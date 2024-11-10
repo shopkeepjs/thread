f4a31cc7086b25d46e012ce9ade728228b6a05f2
Derek <me@chancellor.tech>
2024-11-10 10:41:34 -0600

    removing files

--- test.sh
+++
@@ -1,5 +0,0 @@
-npx jsr publish > file.txt
-echo $OUTPUT
-# if grep -q "Skipping, already published" <<< $OUTPUT; then
-# echo "Error: The package has already been published. Please increment the version number."
-# exit 1
